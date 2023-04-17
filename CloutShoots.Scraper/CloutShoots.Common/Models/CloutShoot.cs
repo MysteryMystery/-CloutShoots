e90d6c474d29baf04aa4d087f733242fb327917b
James Attfield <jamesattfield98@gmail.com>
2023-04-17 19:35:03 +0100

    add initial site

---
+++ CloutShoots.Scraper/CloutShoots.Common/Models/CloutShoot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Text;
+using System.Web;
+
+namespace CloutShoots.Common.Models
+{
+    public class CloutShoot
+    {
+        [JsonIgnore]
+        public Guid Guid { get; set; }
+
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        public string NormalisedName { get => Name.Normalize(); }
+
+        public DateTime Date { get; set; }
+
+        public string? MapUrl { get; set; }
+        public string? FormUrl { get; set; }
+    }
+}
---
+++ CloutShoots.Scraper/CloutShoots.Scraper/Scraper.cs
@@ -0,0 +1,88 @@
+﻿using CloutShoots.Common.Models;
+using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CloutShoots.Scraper
+{
+    internal class Scraper
+    {
+        private List<CloutShoot> _cloutShoots = new();
+
+        public string SaveLocation { get; set; } = "/shoots";
+        public string SaveFile { get; set; } = "CloutShoots.json";
+
+        public Scraper() { 
+        }
+
+        public async Task<string> Get(string url)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public void ParseShoots(string html)
+        {
+            HtmlDocument htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var trs = htmlDocument.DocumentNode.Descendants("tr")
+                .Where(e => e.GetAttributeValue("class", "").Equals("dsil2C wixui-table__row"))
+                .ToList();
+
+            foreach (var tr in trs)
+            {
+                string name = tr.Descendants("div")
+                    .Where(e => e.GetAttributeValue("class", "").Equals("tY1czp") && !e.InnerHtml.Equals("Entry"))
+                    .First().InnerText;
+
+                string date = tr.Descendants("div")
+                    .Where(e => e.GetAttributeValue("class", "").Equals("L0MOmM") && !e.InnerHtml.Equals("Entry"))
+                    .First().InnerText;
+                DateTime dateTime = DateTime.Parse(date);
+
+                var links = tr.Descendants("a")
+                    .Where(e => !string.IsNullOrEmpty(e.GetAttributeValue("href", "")));
+                string mapUrl = links.First()
+                    .GetAttributeValue("href", null);
+
+                string? formUrl = links.Skip(1)
+                    .FirstOrDefault()
+                    ?.GetAttributeValue("href", null);
+
+
+
+                _cloutShoots.Add(new CloutShoot
+                {
+                    Name = name,
+                    MapUrl = mapUrl,
+                    FormUrl = formUrl,
+                    Date = dateTime
+                });
+            }
+        }
+
+        public void Save()
+        {
+            if (!Directory.Exists(SaveLocation))
+            {
+                var dinfo = Directory.CreateDirectory(SaveLocation);
+                Console.WriteLine("Created: " + dinfo.FullName);
+            }
+
+            string json = JsonSerializer.Serialize(this._cloutShoots);
+            using (StreamWriter streamWriter = new StreamWriter(SaveLocation + "/" + SaveFile))
+            {
+                streamWriter.Write(json);
+            }
+        }
+    }
+}
